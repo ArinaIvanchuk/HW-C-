2bfd90bf5e84d3cf1102ff6cc2c23090028dcb7e
Arina <arina_ivanchyk@mail.ru>
2022-08-05 19:21:35 +0300

    решение задачи 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишиите программу, которая на вход принимает два числа и выдает какое число больше, а какое меньше
+Console.Write("введите число a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите число b");
+int b = Convert.ToInt32(Console.ReadLine());
+if(a < b)
+{
+    Console.Write("b больше a");
+}
+if(b < a)
+{
+    Console.Write("a больше b");
+}
+if(a == b)
+{
+    Console.Write("a и b равны");
+}
+
+
+
+
